82963eb6a34fd088485c843a6767b649b538e9c5
Josip Igrec <josip@igr.ec>
2023-03-16 11:53:00 +0100

    Improve cli args

--- src/args/mod.rs
+++ src/args/mod.rs
@@ -3,11 +3,20 @@ use once_cell::sync::Lazy;
 use std::path;
 
 #[derive(Debug, Clone, Parser)]
+/// Media downloader and "fixer"
+///
+/// Downloads media from various websites.
+/// Transcodes and crops files if necessary.
 pub struct Args {
-    #[arg(short, long, default_value = None)]
+    /// Path to the yt-dlp executable
+    #[arg(long, default_value = None)]
     pub yt_dlp_path: Option<path::PathBuf>,
-    #[arg(short, long, default_value = None)]
+
+    /// The directory to save memes to. If not provided, $HOME/MEMES will be used
+    #[arg(short='d', long, default_value = None)]
     pub memes_directory: Option<path::PathBuf>,
+
+    /// The URL to download media from
     #[arg(default_value = None)]
     pub download_url: Option<String>,
 }

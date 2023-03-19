c25bfdcf9dd7069f57ffb48f712727bf09b68e67
Josip Igrec <josip@igr.ec>
2023-03-19 12:08:06 +0100

    Add file name fixer

--- src/fixers/file_extensions.rs
+++ src/fixers/file_extensions.rs
@@ -31,6 +31,8 @@ pub fn fix_file_extension(file_path: &PathBuf) -> FixerReturn {
     debug!("Inferred file extension: {:?}", file_ext);
 
     let new_file_path = file_path.with_extension(file_ext);
+
+    debug!("Renaming file from {file_path:?} to {new_file_path:?}");
     match fs::rename(file_path, &new_file_path) {
         Ok(_) => Ok(new_file_path),
         Err(e) => Err(format!("Failed to rename file: {e:?}")),
--- src/fixers/mod.rs
+++ src/fixers/mod.rs
@@ -4,6 +4,7 @@ use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
 
 pub mod crop;
 pub mod file_extensions;
+pub mod file_name;
 pub mod video_formats;
 
 #[allow(clippy::module_name_repetitions)]
@@ -12,6 +13,7 @@ pub fn fix_files(paths: &Vec<PathBuf>) -> Result<Vec<PathBuf>, String> {
         .par_iter()
         .map(|path| {
             let path = file_extensions::fix_file_extension(path)?;
+            let path = file_name::fix_file_name(&path)?;
             let path = video_formats::convert_file_into_known(&path)?;
             let path = crop::auto_crop_video(&path)?;
             Ok(path)

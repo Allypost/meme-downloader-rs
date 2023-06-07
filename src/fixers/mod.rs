424d7d59d7a05811ecadee2f31f681846f0703a2
Josip Igrec <josip@igr.ec>
2023-06-07 04:37:39 +0200

    Fixup fixers module handler

--- src/fixers/mod.rs
+++ src/fixers/mod.rs
@@ -8,16 +8,24 @@ pub mod file_name;
 pub mod video_formats;
 
 pub fn fix_files(paths: &Vec<PathBuf>) -> Result<Vec<PathBuf>, String> {
+    let fixers: Vec<Fixer> = vec![
+        file_extensions::fix_file_extension,
+        file_name::fix_file_name,
+        video_formats::convert_file_into_known,
+        crop::auto_crop_video,
+    ];
+
     paths
         .par_iter()
         .map(|path| {
-            let path = file_extensions::fix_file_extension(path)?;
-            let path = file_name::fix_file_name(&path)?;
-            let path = video_formats::convert_file_into_known(&path)?;
-            let path = crop::auto_crop_video(&path)?;
-            Ok(path)
+            let mut p = path.clone();
+            for filter in &fixers {
+                p = filter(path)?;
+            }
+            Ok(p)
         })
         .collect()
 }
 
 type FixerReturn = Result<PathBuf, String>;
+type Fixer = fn(&PathBuf) -> FixerReturn;

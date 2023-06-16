48b8efa62557d8bc96425b366e3300b3ad94bffc
Josip Igrec <josip@igr.ec>
2023-06-16 05:32:35 +0200

    Use thread-aware time id in temp_dir helper

--- crates/helpers/src/dirs.rs
+++ crates/helpers/src/dirs.rs
@@ -3,10 +3,10 @@ use std::{fs, path::PathBuf};
 use anyhow::anyhow;
 use config::CONFIGURATION;
 
-use crate::id::time_id;
+use crate::id::time_thread_id;
 
 pub fn create_temp_dir() -> anyhow::Result<PathBuf> {
-    let id = time_id().map_err(|e| anyhow!("Error while getting time: {e:?}"))?;
+    let id = time_thread_id().map_err(|e| anyhow!("Error while getting time: {e:?}"))?;
     let temp_dir = CONFIGURATION.cache_dir().join(id);
 
     fs::create_dir_all(&temp_dir)?;

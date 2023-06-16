751a24d5a193484371a380ce1ebaad7ee7280733
Josip Igrec <josip@igr.ec>
2023-06-16 05:29:24 +0200

    Add time_id with thread info

--- crates/helpers/src/id.rs
+++ crates/helpers/src/id.rs
@@ -1,12 +1,34 @@
+use anyhow::Result;
 use base64::Engine;
-use std::time;
+use std::{process, thread, time};
 
-pub fn time_id() -> Result<String, time::SystemTimeError> {
-    let now_ns = time::SystemTime::now()
+fn now_ns() -> Result<u128> {
+    let ns = time::SystemTime::now()
         .duration_since(time::UNIX_EPOCH)?
         .as_nanos();
 
-    let id = base64::engine::general_purpose::STANDARD_NO_PAD.encode(now_ns.to_string());
+    Ok(ns)
+}
 
-    Ok(id)
+#[allow(clippy::unnecessary_wraps)]
+fn encode<T: AsRef<[u8]>>(data: T) -> Result<String> {
+    let encoded = base64::engine::general_purpose::STANDARD_NO_PAD.encode(data);
+
+    Ok(encoded)
+}
+
+pub fn time_id() -> Result<String> {
+    let ns = now_ns()?;
+
+    encode(ns.to_string())
+}
+
+pub fn time_thread_id() -> Result<String> {
+    let thread_id = thread::current().id();
+    let process_id = process::id();
+    let ns = now_ns()?;
+
+    let id = format!("{ns}-{process_id}-{thread_id:?}");
+
+    encode(id)
 }
